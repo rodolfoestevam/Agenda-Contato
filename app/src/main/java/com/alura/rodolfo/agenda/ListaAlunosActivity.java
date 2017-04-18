faa6a44c8bd0a71071effc5d5e84b9010f16e8e1
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-04-17 21:25:14 -0300

    Tratamento de threads com ASyncTask - EnviaAlunosTask; [xml] - menu_lista_alunos.xml - Teste de botão com dados via JSONStringer WebClient - configuracao do json via post method AndroidManifest - atualizacão de permissão de internet AlunoConverter - criado package converter para converter aluno em JSon

--- app/src/main/java/com/alura/rodolfo/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/alura/rodolfo/agenda/ListaAlunosActivity.java
@@ -7,11 +7,10 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.MenuItemImpl;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.view.MenuItem;
@@ -21,8 +20,6 @@ import com.alura.rodolfo.agenda.dao.AlunoDAO;
 import com.alura.rodolfo.agenda.modelo.Aluno;
 
 import java.util.List;
-
-import static android.Manifest.permission.CALL_PHONE;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -77,6 +74,23 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         carregaLista();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_lista_alunos, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_enviar_notas:
+                new EnviaAlunoTask(this).execute();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
