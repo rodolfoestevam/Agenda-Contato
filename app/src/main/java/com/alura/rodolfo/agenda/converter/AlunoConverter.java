00fd5fbed1080325f8ee980fcb46eb37acb00d6e
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2017-05-03 12:41:59 -0300

    Removida a classe InsereAlunoTask, agora estamos utilizando a biblioteca do retrofit para fazer todo o trabalho do asynctask Adicionado classe Retrofitinicializador para criar nossa baseUrl Adicionamos as dependencias retrofit2 e JacksonJson

--- app/src/main/java/com/alura/rodolfo/agenda/converter/AlunoConverter.java
+++ app/src/main/java/com/alura/rodolfo/agenda/converter/AlunoConverter.java
@@ -12,29 +12,30 @@ import java.util.List;
  */
 
 public class AlunoConverter {
-    public String converteParaJSON (List<Aluno> alunos) {
-        JSONStringer js = new JSONStringer();
-        try {
-            js.object().key("list").array()
-                    .object().key("aluno").array();
 
-            for (Aluno aluno : alunos) {
-                js.object()
-                        .key("id").value(aluno.getId())
-                        .key("nome").value(aluno.getNome())
-                        .key("telefone").value(aluno.getTelefone())
-                        .key("endereco").value(aluno.getEndereco())
-                        .key("site").value(aluno.getSite())
-                        .key("nota").value(aluno.getNota())
-                        .endObject();
-            }
-            return js.endArray().endObject()
-                    .endArray().endObject().toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+  public String converteParaJSON(List<Aluno> alunos) {
+    JSONStringer js = new JSONStringer();
+    try {
+      js.object().key("list").array()
+          .object().key("aluno").array();
 
-     return "";
+      for (Aluno aluno : alunos) {
+        js.object()
+            .key("id").value(aluno.getId())
+            .key("nome").value(aluno.getNome())
+            .key("telefone").value(aluno.getTelefone())
+            .key("endereco").value(aluno.getEndereco())
+            .key("site").value(aluno.getSite())
+            .key("nota").value(aluno.getNota())
+            .endObject();
+      }
+      return js.endArray().endObject()
+          .endArray().endObject().toString();
+    } catch (JSONException e) {
+      e.printStackTrace();
     }
+
+    return "";
+  }
 
 }
